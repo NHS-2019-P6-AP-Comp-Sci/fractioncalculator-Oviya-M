0c85fa94b3fba9a5f4c1df4d884d26dcd84950a6
s-muralidharano <s-muralidharano@53S500037009.it.bsd405.org>
2019-11-14 20:54:46 -0800

    Check Point 1 is done.

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -3,13 +3,21 @@
  */
 
 package fracCalc;
-
+import java.util.Scanner;
 public class FracCalc {
 
     public static void main(String[] args)
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
-
+    	//INTRODUCTION AND USER INPUT
+    	System.out.print("Please enter in a equation that adds, subtracts, multiplies");
+    	System.out.println("or divides 2 fractions (can be proper, improper or mixed): ");
+    	Scanner userInput = new Scanner(System.in);
+    	String user = userInput.nextLine();
+    	
+    	//CALLS, STORES AND PRINTS RETURN VALUE OF FUNCTION produceAnswer
+    	String x = produceAnswer(user);
+    	System.out.println(x);
     }
 
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -20,11 +28,25 @@ public class FracCalc {
     //
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
+    public static String produceAnswer(String input) {
         // TODO: Implement this function to produce the solution to the input
-
-        return "";
+    	//VARIABLES FOR EACH STRING
+    	String operand1 = "";
+    	String operator = "";
+    	String operand2 = "";
+    	
+    	//FIND INDEX OF SPACE
+    	int spcIndex = input.indexOf(" ");
+    	//USE INDEX OF SPACE TO RETURN SUBSTRINGS OF THE "input" PARAMETER
+    	operand1 = input.substring(0, spcIndex);
+    	operator = input.substring(spcIndex+1, spcIndex+2);
+    	int opIndex = input.indexOf(operator);
+    	operand2 = input.substring(opIndex+2);
+    	//Test
+    	//System.out.println("operand1 = " + operand1);
+    	//System.out.println("operator = " + operator);
+    	//System.out.println("operand1 = " + operand2);
+        return operand2;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
